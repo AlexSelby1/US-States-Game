f980cdbae69d5f491bdc58fa7508f9f980405c3d
Alex Selby <alex.selby@hotmail.com>
2021-10-12 16:57:31 +0100

    updated to keep the programming running until complete or exit

--- main.py
+++ main.py
@@ -10,22 +10,27 @@ turtle.shape(img)
 
 data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
+guessed_states = []
+states_to_learn = []
 
 
-answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name?")
+while len(guessed_states) < 50:
+    
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct", prompt="What is another state's name?").title()
 
-if answer_state in states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.pu()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x),int(state_data.y))
-    t.write(answer_state)
+    if answer_state == "Exit":
+        break
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        t.write(answer_state)
 
-# Used to get x y coords of an image
-# def get_mouse_click_coor(x,y):
-#     print(x,y)
-# turtle.onscreenclick(get_mouse_click_coor)
-
-turtle.exitonclick()
+    # Used to get x y coords of an image
+    # def get_mouse_click_coor(x,y):
+    #     print(x,y)
+    # turtle.onscreenclick(get_mouse_click_coor)
 
