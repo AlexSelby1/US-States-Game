92b214706b7b89ffc2b70b31c9bd9f5f7acd11ca
Alex Selby <alex.selby@hotmail.com>
2021-10-12 16:46:33 +0100

    Adding user input and displaying named states on screen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from turtle import Turtle, Screen
+import turtle, pandas
 
-turtle = Turtle()
-screen = Screen()
+screen = turtle.Screen()
 
 screen.title("U.S States Game")
 img = "blank_states_img.gif"
@@ -9,4 +8,24 @@ screen.addshape(img)
 
 turtle.shape(img)
 
-screen.exitonclick()
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+
+
+answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name?")
+
+if answer_state in states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.pu()
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x),int(state_data.y))
+    t.write(answer_state)
+
+# Used to get x y coords of an image
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.exitonclick()
+
